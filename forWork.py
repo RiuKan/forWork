bf4717ca00820b97dbaca18ac722b54bae1de5d7
Riukan <rk850323@naver.com>
2020-09-20 23:41:40 +0900

    일단 5개 기관 , 버튼으로 구현
    기존 코드 수정하고, 다시 맞춘 후
    설정 구현할 예정.

--- forWork.py
+++ forWork.py
@@ -37,11 +37,19 @@ class Ui_MainWindow(object):
         self.gridLayout_3.setContentsMargins(0, 0, 0, 0)
         self.gridLayout_3.setSpacing(2)
         self.gridLayout_3.setObjectName("gridLayout_3")
+        self.label = QLabel(self.gridLayoutWidget)
+        self.label.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
+        self.label.setObjectName("label")
+        self.gridLayout_3.addWidget(self.label, 0, 0, 1, 1)
         self.gridLayout = QGridLayout()
         self.gridLayout.setContentsMargins(-1, -1, -1, 0)
         self.gridLayout.setHorizontalSpacing(2)
         self.gridLayout.setVerticalSpacing(4)
         self.gridLayout.setObjectName("gridLayout")
+        self.label_3 = QLabel(self.gridLayoutWidget)
+        self.label_3.setText("")
+        self.label_3.setObjectName("label_3")
+        self.gridLayout.addWidget(self.label_3, 0, 0, 1, 1)
         self.label_4 = QLabel(self.gridLayoutWidget)
         self.label_4.setText("")
         self.label_4.setObjectName("label_4")
@@ -50,42 +58,8 @@ class Ui_MainWindow(object):
         self.label_5.setText("")
         self.label_5.setObjectName("label_5")
         self.gridLayout.addWidget(self.label_5, 0, 2, 1, 1)
-        self.label_3 = QLabel(self.gridLayoutWidget)
-        self.label_3.setText("")
-        self.label_3.setObjectName("label_3")
-        self.gridLayout.addWidget(self.label_3, 0, 0, 1, 1)
         self.gridLayout.setColumnStretch(0, 1)
         self.gridLayout_3.addLayout(self.gridLayout, 0, 1, 1, 1)
-        self.label_2 = QLabel(self.gridLayoutWidget)
-        self.label_2.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
-        self.label_2.setObjectName("label_2")
-        self.gridLayout_3.addWidget(self.label_2, 1, 0, 1, 1)
-        self.gridLayout_2 = QGridLayout()
-        self.gridLayout_2.setHorizontalSpacing(2)
-        self.gridLayout_2.setVerticalSpacing(5)
-        self.gridLayout_2.setObjectName("gridLayout_2")
-        self.label_6 = QLabel(self.gridLayoutWidget)
-        self.label_6.setText("")
-        self.label_6.setObjectName("label_6")
-        self.gridLayout_2.addWidget(self.label_6, 0, 0, 1, 1)
-        self.label_7 = QLabel(self.gridLayoutWidget)
-        self.label_7.setText("")
-        self.label_7.setObjectName("label_7")
-        self.gridLayout_2.addWidget(self.label_7, 0, 1, 1, 1)
-        self.label_8 = QLabel(self.gridLayoutWidget)
-        self.label_8.setText("")
-        self.label_8.setObjectName("label_8")
-        self.gridLayout_2.addWidget(self.label_8, 0, 2, 1, 1)
-        self.label_9 = QLabel(self.gridLayoutWidget)
-        self.label_9.setText("")
-        self.label_9.setObjectName("label_9")
-        self.gridLayout_2.addWidget(self.label_9, 0, 3, 1, 1)
-        self.gridLayout_2.setColumnStretch(2, 1)
-        self.gridLayout_3.addLayout(self.gridLayout_2, 1, 1, 1, 1)
-        self.label = QLabel(self.gridLayoutWidget)
-        self.label.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
-        self.label.setObjectName("label")
-        self.gridLayout_3.addWidget(self.label, 0, 0, 1, 1)
         self.dateEdit = QDateEdit(self.centralwidget)
         self.dateEdit.setGeometry(QRect(120, 30, 171, 51))
         self.dateEdit.setAlignment(Qt.AlignCenter)
@@ -99,20 +73,74 @@ class Ui_MainWindow(object):
         self.pushButton_sym = QPushButton(self.centralwidget)
         self.pushButton_sym.setGeometry(QRect(10, 110, 41, 51))
         self.pushButton_sym.setObjectName("pushButton_sym")
-        self.pushButton_bog = QPushButton(self.centralwidget)
-        self.pushButton_bog.setGeometry(QRect(10, 430, 41, 51))
-        self.pushButton_bog.setObjectName("pushButton_bog")
-        self.pushButton_web = QPushButton(self.centralwidget)
-        self.pushButton_web.setGeometry(QRect(880, 30, 151, 28))
-        self.pushButton_web.setObjectName("pushButton_web")
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QRect(880, 30, 151, 28))
+        self.pushButton.setObjectName("pushButton")
+        self.select_sym = QPushButton(self.centralwidget)
+        self.select_sym.setGeometry(QRect(310, 40, 71, 41))
+        self.select_sym.setObjectName("select_sym")
+        self.select_bog = QPushButton(self.centralwidget)
+        self.select_bog.setGeometry(QRect(400, 40, 81, 41))
+        self.select_bog.setObjectName("select_bog")
+        self.select_hyeop = QPushButton(self.centralwidget)
+        self.select_hyeop.setGeometry(QRect(500, 40, 71, 41))
+        self.select_hyeop.setObjectName("select_hyeop")
+        self.button_tong = QPushButton(self.centralwidget)
+        self.button_tong.setGeometry(QRect(590, 40, 131, 41))
+        self.button_tong.setObjectName("button_tong")
+        self.button_ye = QPushButton(self.centralwidget)
+        self.button_ye.setGeometry(QRect(730, 40, 121, 41))
+        self.button_ye.setObjectName("button_ye")
+        self.button_sym_list = QPushButton(self.centralwidget)
+        self.button_sym_list.setGeometry(QRect(310, 10, 71, 21))
+        self.button_sym_list.setText("")
+        self.button_sym_list.setObjectName("button_sym_list")
+        self.button_bog_list = QPushButton(self.centralwidget)
+        self.button_bog_list.setGeometry(QRect(400, 10, 81, 21))
+        self.button_bog_list.setText("")
+        self.button_bog_list.setObjectName("button_bog_list")
+        self.button_hyeop_list = QPushButton(self.centralwidget)
+        self.button_hyeop_list.setGeometry(QRect(500, 10, 71, 21))
+        self.button_hyeop_list.setText("")
+        self.button_hyeop_list.setObjectName("button_hyeop_list")
+        self.button_tong_list = QPushButton(self.centralwidget)
+        self.button_tong_list.setGeometry(QRect(590, 10, 131, 21))
+        self.button_tong_list.setText("")
+        self.button_tong_list.setObjectName("button_tong_list")
+        self.button_ye_list = QPushButton(self.centralwidget)
+        self.button_ye_list.setGeometry(QRect(730, 10, 121, 21))
+        self.button_ye_list.setText("")
+        self.button_ye_list.setObjectName("button_ye_list")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setGeometry(QRect(0, 0, 1141, 26))
         self.menubar.setObjectName("menubar")
+        self.menu_2 = QMenu(self.menubar)
+        self.menu_2.setObjectName("menu_2")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        self.action_2 = QAction(MainWindow)
+        self.action_2.setObjectName("action_2")
+        self.action_4 = QAction(MainWindow)
+        self.action_4.setObjectName("action_4")
+        self.action_5 = QAction(MainWindow)
+        self.action_5.setObjectName("action_5")
+        self.action_6 = QAction(MainWindow)
+        self.action_6.setObjectName("action_6")
+        self.action_8 = QAction(MainWindow)
+        self.action_8.setObjectName("action_8")
+        self.action_9 = QAction(MainWindow)
+        self.action_9.setObjectName("action_9")
+        self.menu_2.addSeparator()
+        self.menu_2.addAction(self.action_2)
+        self.menu_2.addSeparator()
+        self.menu_2.addAction(self.action_8)
+        self.menu_2.addAction(self.action_4)
+        self.menu_2.addAction(self.action_5)
+        self.menu_2.addAction(self.action_9)
+        self.menubar.addAction(self.menu_2.menuAction())
 
         self.retranslateUi(MainWindow)
         QMetaObject.connectSlotsByName(MainWindow)
@@ -120,25 +148,34 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label_2.setText(_translate("MainWindow", "보건복지부"))
         self.label.setText(_translate("MainWindow", "심평원"))
         self.pushButton_today.setText(_translate("MainWindow", "오늘"))
         self.pushButton_sym.setText(_translate("MainWindow", "목록"))
-        self.pushButton_bog.setText(_translate("MainWindow", "목록"))
-        self.pushButton_web.setText(_translate("MainWindow", "브라우저 파일 지정"))
-
+        self.pushButton.setText(_translate("MainWindow", "브라우저 파일 지정"))
+        self.select_sym.setText(_translate("MainWindow", "심평원"))
+        self.select_bog.setText(_translate("MainWindow", "보건복지부"))
+        self.select_hyeop.setText(_translate("MainWindow", "보건협회"))
+        self.button_tong.setText(_translate("MainWindow", "보건의료통합자원"))
+        self.button_ye.setText(_translate("MainWindow", "보건복지부 예고"))
+        self.menu_2.setTitle(_translate("MainWindow", "설정"))
+        self.action_2.setText(_translate("MainWindow", "브라우저 파일 지정"))
+        self.action_4.setText(_translate("MainWindow", "버그 리포트"))
+        self.action_5.setText(_translate("MainWindow", "제작자"))
+        self.action_6.setText(_translate("MainWindow", "버전 확인"))
+        self.action_8.setText(_translate("MainWindow", "버전 확인"))
+        self.action_9.setText(_translate("MainWindow", "커스토마이징"))
 
 class MyWindow(QMainWindow, Ui_MainWindow):
 
     def __init__(self):
         super().__init__()
         self.setUI()
-        self.getList(strftime('%Y-%m-%d', localtime(time())))
+        # self.getList(strftime('%Y-%m-%d', localtime(time())))
         self.dateEdit.setDate(QDate.currentDate())
         self.pushButton_today.clicked.connect(lambda: self.today_button_clicked())
         self.pushButton_sym.clicked.connect(lambda : self.sym_button_clicked())
-        self.pushButton_bog.clicked.connect(lambda : self.bog_button_clicked())
-        self.pushButton_web.clicked.connect(lambda: self.web_button_clicked())
+        # self.pushButton_bog.clicked.connect(lambda : self.bog_button_clicked())
+        # self.pushButton_web.clicked.connect(lambda: self.web_button_clicked())
         global web_localURL
         web_localURL = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
         webbrowser.register("wb", None,webbrowser.BackgroundBrowser(web_localURL))
@@ -182,10 +219,20 @@ class MyWindow(QMainWindow, Ui_MainWindow):
 # TODO 파일을 다운로드 받아 바로 열 수 있도록, 파일 선택 가능하도록
 # TODO 링크와 내용 분리(팝업으로 내용(표 나타내는법 알아내야함) 보여주고, 링크는 버튼으로 놔두기)
 # TODO bug report 메일과 보내는 양식, (메일이 아니라 프로그램에서 보내지게 할 수 없나, bug 발생시 해당 로그 보내는 방식 알아보기)(아마 에러처리 하고 그 조건에 대입하는 것일듯)
-
+# TODO log 남기는 방법, 최근 5번의 조작을 시간과 함께 순서 남기기, error 로그랑 같이 남기기
+# TODO 시행일은 버튼 누르면 분석해서 버튼의 텍스트가 바뀌도록.(~추정)
 # TODO 기억할 것은 최소한 바로가기 해놓은 것 보다 편리해야하고, 뇌의 처리과정을 단축시켜줘야한다.
 # TODO 오늘 공시가 올라온 사이트가 뭘까 -> 다 켜는것이 아닌, 버튼의 색깔로 한번에 구분
 # TODO 사이트를 다 켜고도, 날짜를 구분해 확인해야하는 번거로움, 해당 날짜만 뽑아서 보여주기.
+# TODO 오늘 올라온 버튼은 따로 표시하고, 각각의 날짜에 해당하는 공시 있는애들 표시 해주는 것도 괜찮을듯
+# TODO 라인 구분해서 보기
+# TODO 한번에 보기 따로따로 보기, 텍스트 크기 변경 가능하도록
+# TODO 실시간 체크 기능 도입(오늘 날짜누르면 30분카운트, 30분 뒤에도 오늘이면 다시 불러오기(충돌방지 기능넣기))
+# TODO push button 순서 조정 기능 어떨까
+# TODO 종료시 설정 저장. (설정 저장 폴더 지정 기능 도입)
+# TODO 커스토마이징 기능 도입(위치나 크기, 글자크기 등을 수정하는 ui 보여주기)
+# TODO HTML 파일 입력시 원하는 부위 주소 찾아주는 기능(타부서 변경 가능하도록)
+# TODO 업데이트 구현 (google app engine 이용)
             roots.destroy()
 
     def sym_button_clicked(self):
@@ -197,68 +244,7 @@ class MyWindow(QMainWindow, Ui_MainWindow):
         # TODO 중복되는 함수들 한 변수로 받고 넘겨주기,strftime('%Y-%m-%d', localtime(time()))
 
 
-    def loop_bog(self, date):
-        response_bog = get("http://www.mohw.go.kr/react/jb/sjb0406ls.jsp?PAR_MENU_ID=03&MENU_ID=030406&page=1")
-        soup_bog = BeautifulSoup(response_bog.text, 'html.parser')
-        texts_bog = soup_bog.select("table>tbody>tr")
-        global list_all_bog
-        list_all_bog = []
-        for i in range(0, 30, 2):
-                # "%s" % time.strftime('%Y-%m-%d', time.localtime(time.time()))
 
-            if texts_bog[i].select("td")[4].get_text() == date:
-                list = []
-                for s in range(1, 6):
-                    if s == 2 or s == 3 or s == 4:
-
-                        list.append(texts_bog[i].select("td")[s].get_text())
-                    elif s == 1:
-                        rep = texts_bog[i].select("td")[s].get_text()
-                        rep = rep.lstrip("\r\n\t")
-                        rep = rep.rstrip("\t\n\r")
-
-                        list.append(rep)
-                    elif s == 5:
-
-
-                            site_number = soup_bog.select("table>tbody>tr>td>a")[i]['onclick']
-                            site_number = site_number.replace("","")
-                            site_number = site_number.rstrip(";)'")
-                            site_number = site_number.lstrip("moveAction('")
-                            list.append(site_number)
-
-
-
-
-
-
-                list_all_bog.append(list)
-        self.gridLayout_2.setHorizontalSpacing(len(list_all_bog))
-        for i in reversed(range(self.gridLayout_2.count())):
-            self.gridLayout_2.itemAt(i).widget().deleteLater()
-        if len(list_all_bog) == 0:
-            cell = MakingLabel("공시 없음")
-
-            self.gridLayout_2.addWidget(cell, 0, 0)
-
-        else:
-            for x in range(0, len(list_all_bog)):
-                for y in range(0, 5):
-                    if y == 0 or y ==1 or y == 3:
-                        cell = MakingLabel(list_all_bog[x][y])
-
-
-                        self.gridLayout_2.addWidget(cell, x, y)
-                    elif y == 2:
-                        cell = MakingLabel('<a href = "http://www.mohw.go.kr/react/jb/sjb0406vw.jsp?PAR_MENU_ID=03&MENU_ID=030406&SEARCHKEY=&SEARCHVALUE=&page=1&CONT_SEQ=%s&nowRowCnt=15">%s</a>' % (list_all_bog[x][4], list_all_bog[x][y]))
-
-                        cell.linkActivated.connect(lambda: self.link_activated("bog"))
-                        self.gridLayout_2.addWidget(cell, x, y)
-                    elif y == 4:
-                        button = MakingButton("문서")
-
-                        button.clicked.connect(lambda:self.buttonClicked("bog"))
-                        self.gridLayout_2.addWidget(button,x,y)
     def link_activated(self,str):
         if str == "bog":
             a = self.sender().text()
@@ -436,7 +422,68 @@ class MyWindow(QMainWindow, Ui_MainWindow):
 # 과거 고시 모두 긁기
 
 
-
+    # def loop_bog(elf, date):
+    #     response_bog = get("http://www.mohw.go.kr/react/jb/sjb0406ls.jsp?PAR_MENU_ID=03&MENU_ID=030406&page=1")
+    #     soup_bog = BeautifulSoup(response_bog.text, 'html.parser')
+    #     texts_bog = soup_bog.select("table>tbody>tr")
+    #     global list_all_bog
+    #     list_all_bog = []
+    #     for i in range(0, 30, 2):
+    #             # "%s" % time.strftime('%Y-%m-%d', time.localtime(time.time()))
+    #
+    #         if texts_bog[i].select("td")[4].get_text() == date:
+    #             list = []
+    #             for s in range(1, 6):
+    #                 if s == 2 or s == 3 or s == 4:
+    #
+    #                     list.append(texts_bog[i].select("td")[s].get_text())
+    #                 elif s == 1:
+    #                     rep = texts_bog[i].select("td")[s].get_text()
+    #                     rep = rep.lstrip("\r\n\t")
+    #                     rep = rep.rstrip("\t\n\r")
+    #
+    #                     list.append(rep)
+    #                 elif s == 5:
+    #
+    #
+    #                         site_number = soup_bog.select("table>tbody>tr>td>a")[i]['onclick']
+    #                         site_number = site_number.replace("","")
+    #                         site_number = site_number.rstrip(";)'")
+    #                         site_number = site_number.lstrip("moveAction('")
+    #                         list.append(site_number)
+    #
+    #
+    #
+    #
+    #
+    #
+    #             list_all_bog.append(list)
+    #     self.gridLayout_2.setHorizontalSpacing(len(list_all_bog))
+    #     for i in reversed(range(self.gridLayout_2.count())):
+    #         self.gridLayout_2.itemAt(i).widget().deleteLater()
+    #     if len(list_all_bog) == 0:
+    #         cell = MakingLabel("공시 없음")
+    #
+    #         self.gridLayout_2.addWidget(cell, 0, 0)
+    #
+    #     else:
+    #         for x in range(0, len(list_all_bog)):
+    #             for y in range(0, 5):
+    #                 if y == 0 or y ==1 or y == 3:
+    #                     cell = MakingLabel(list_all_bog[x][y])
+    #
+    #
+    #                     self.gridLayout_2.addWidget(cell, x, y)
+    #                 elif y == 2:
+    #                     cell = MakingLabel('<a href = "http://www.mohw.go.kr/react/jb/sjb0406vw.jsp?PAR_MENU_ID=03&MENU_ID=030406&SEARCHKEY=&SEARCHVALUE=&page=1&CONT_SEQ=%s&nowRowCnt=15">%s</a>' % (list_all_bog[x][4], list_all_bog[x][y]))
+    #
+    #                     cell.linkActivated.connect(lambda: self.link_activated("bog"))
+    #                     self.gridLayout_2.addWidget(cell, x, y)
+    #                 elif y == 4:
+    #                     button = MakingButton("문서")
+    #
+    #                     button.clicked.connect(lambda:self.buttonClicked("bog"))
+    #                     self.gridLayout_2.addWidget(button,x,y)
     def setUI(self):
         self.setupUi(self)
 
@@ -446,6 +493,7 @@ if __name__ == "__main__":
     import sys
 
     app = QApplication(sys.argv)
+
     myApp = MyWindow()
     myApp.show()
     app.exec_()
